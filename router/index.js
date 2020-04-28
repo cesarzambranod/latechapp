9c96bd2aab85d3a07ba4a76b1aa020c808f2bcdb
Cesar Zambrano Delgadillo <cesarzambranod@gmail.com>
2020-04-28 16:24:22 -0300

    change

--- router/index.js
+++ router/index.js
@@ -1,29 +1,34 @@
 const {Router}=require ('express');
 const router=Router();
 const admin = require('firebase-admin');
+var estado=false;
 var serviceAccount = require("../../latechfactory-firebase-adminsdk-slk9a-80d0065e3f.json");
+const app = expres();
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     datebaseURL:'https://latechfactory.firebaseio.com/database/'
+}), (err=>{
+    if (err) res.status(403).send({message:`No posee autentificacion ${err}`})
+    res.status(500).send({message:`Autentificacion exitosa`})
+    estado=true;
 });
-const db= admin.database();
 
+const db= admin.database();
 router.get('/',(req,res)=> {
     db.ref(producto).once('value', (snapshot)=>{
         const data= snapshot.val()
-        res.render('index', {producto:data});
+        res.status(200).render('index', {producto:data});
     })
 
 });
-
-router.post('/nuevo-producto', (req,res)=>{
+if (estado=true){router.post('/nuevo-producto', (req,res)=>{
     const nuevoProducto={
         nombreProducto:req.body.nombreProducto,
         precioProducto:req.body.precioProducto
     }
     db.ref(producto).push(nuevoProducto)
-    res.redirect('/');
+    res.status(200).redirect('/');
 });
 
 router.put('/actualizar-producto/:id',(req,res)=>{
@@ -32,13 +37,16 @@ router.put('/actualizar-producto/:id',(req,res)=>{
         precioProducto:req.body.precioProducto
     }
     db.ref('producto/'+ req.params.id).update;
-    res.redirect('/');
+    res.status(200).redirect('/');
 })
 
-router.get('/borrar-producto/:id',(res,req)=>{
+router.delete('/borrar-producto/:id',(res,req)=>{
 db.ref('producto/'+ req.params.id).remove;
-res.redirect('/');
+res.status(200).redirect('/');
 })
+}
+
+
 
 
 module.exports=router;
